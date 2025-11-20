152871006aa1008db897cd19f7326c8aef2545b9
Samuel <samuel05015@gmail.com>
2025-11-20 01:00:09 -0300

    fix: Corrigir base path para desenvolvimento local

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/radiant-you-style/",
+  base: mode === "production" ? "/radiant-you-style/" : "/",
 }));
